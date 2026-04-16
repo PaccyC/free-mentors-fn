f7ee535a7c3bf6506a665c65083eeff78d4ef9fb
PaccyC <abayisengaaimepacifique@gmail.com>
2026-04-16 15:48:33 +0200

    [init]:initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+
+
+  return (
+    <div className="h-[90vh] w-full bg-red-300">
+     <h1 className="text-white text-3xl">Testing Tailwindcss</h1>
+    </div>
+  )
+}
+
+export default App
